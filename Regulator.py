726b315b69d80a7d4979c8a32cf73d0cd2a76bfe
DenchicEz <65415569+denchicez@users.noreply.github.com>
2022-04-17 10:44:28 +0300

    fx Regulator

--- Regulator.py
+++ Regulator.py
@@ -8,19 +8,18 @@ class Regulator:
         self.system = system
         self.threads = {}
 
-    def play(self, id: str, price: str, timedelta: str):  # асинхронная
+    def play(self, id: str, price: str, timedelta: str, delay):  # асинхронная
         result = 0
-        player = Player(id, price, timedelta, self.system)
+        player = Player(id, price, timedelta, delay, self.system)
         while (result == 0):
             result = player.play()
         del self.threads[id]
         self.system.sender.template(result, id)
 
-    def newsession(self, id: str, price: str, timedelta: str):
+    def newsession(self, id: str, price: str, sendmessage: str, delay):
         if (id not in self.threads):
-            self.threads[id] = Process(target=self.play, args=(id, price, timedelta))
+            self.threads[id] = Process(target=self.play, args=(id, price, sendmessage, delay))
             self.threads[id].start()
-            print(self.threads)
         else:
             if self.system.log:
                 print("You're now upd this!")
