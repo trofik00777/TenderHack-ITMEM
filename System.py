1544792eb62ae8b2317957bd885f5cb2e6b3f90c
DenchicEz <65415569+denchicez@users.noreply.github.com>
2022-04-17 13:25:50 +0300

    Update System.py

--- System.py
+++ System.py
@@ -1,87 +1,102 @@
-import requests
+from aiohttp import ClientSession
 
 from Sender import Sender
 
 
 class System:
     @staticmethod
-    def __checkNet():  # проверяем работает ли интернет
-        try:
-            requests.get("https://edu.pp24.dev/ru", timeout=10)
-            return 1  # работает
-        except requests.ConnectionError:
-            return 0  # не работает
+    async def __checkNet():  # проверяем работает ли интернет
+        async with ClientSession() as session:
+            try:
+                async with session.get("https://edu.pp24.dev/ru") as resp:
+                    return 1
+            except:
+                return 0
+
+    async def create(self):
+        async with ClientSession() as session:
+            self.cookies = session.cookie_jar
+        if self.log:
+            print("Start login in account")
+        url = "https://old.edu.pp24.dev/api/Cssp/Authentication/BeginAuthentication?type=Password"
+        while True:
+            if await self.__checkNet():
+                async with ClientSession(cookie_jar=self.cookies) as session:
+                    async with session.get(url) as resp:
+                        if self.log:
+                            print(await resp.text())
+                        self.cookies = session.cookie_jar
+                        token = await resp.text()
+                        token = token[1:-1]
+                        self.error = await self.__login(self.password, self.login, token)
+                        if self.error == 0:
+                            print("Succesfully login")
+                        else:
+                            print("Unseccusfully login")
+            if self.log:
+                print("Problem with internet")
 
     def __init__(self, login: str, password: str, receive_mail, telegram_message, log: bool = False):
         self.active = 1
         self.log = log
+        self.password = password
+        self.login = login
         self.telegram_message = telegram_message
-        self.session = requests.Session()
         self.sender = Sender(receive_mail, telegram_message)
-        if self.log:
-            print("Start login in account")
-        while True:
-            if self.__checkNet():
-                resp = self.session.get(
-                    "https://old.edu.pp24.dev/api/Cssp/Authentication/BeginAuthentication?type=Password")
-                if self.log:
-                    print(resp)
-                break
-            if self.log:
-                print("Problem with internet")
-        token = resp.text
-        token = token[1:-1]
-        self.error = self.__login(password, login, token)
-        if self.error == 0:
-            print("Succesfully login")
-        else:
-            print("Unseccusfully login")
 
     # 0 - successful, -1 - error
-    def __login(self, password: str, login: str, token: str):
+    async def __login(self, password: str, login: str, token: str):
         url = "https://old.edu.pp24.dev/api/Cssp/Authentication/PerformAuthentication"
         data = {"token": token, "operation": "LogIn",
                 "argument": {"values": {"login": f"{login}", "password": f"{password}"}}}
         while True:
-            if self.__checkNet():
-                resp = self.session.post(url, json=data)  # work with this!
+            if await self.__checkNet():
+                async with ClientSession(cookie_jar=self.cookies) as session:
+                    async with session.post(url, json=data) as response:
+                        resp = await response.json()
+                        self.cookies = session.cookie_jar
                 break
             if self.log:
                 print("Problem with internet")
-        resp = resp.json()
         if (resp['isSessionComplete'] == True):
             url = "https://old.edu.pp24.dev/api/Cssp/Authentication/CheckAuthentication"
             while True:
-                if self.__checkNet():
-                    resp = self.session.get(url)
-                    break
+                if await self.__checkNet():
+                    async with ClientSession(cookie_jar=self.cookies) as session:
+                        async with session.get(url) as response:
+                            if self.log:
+                                print(await response.text())
+                            resp = await response.json()
+                            if (resp['isAuthenticated'] == False):
+                                if self.log:
+                                    print("Failed login")
+                                return -1
+                            if self.log:
+                                print("We succesfully login")
+                            self.cookies = session.cookie_jar
+                            return 0
                 if self.log:
                     print("Problem with internet")
-            resp = resp.json()
-            if (resp['isAuthenticated'] == False):
-                if self.log:
-                    print("Failed login")
-                return -1
-            if self.log:
-                print("We succesfully login")
-            return 0
         else:
             if self.log:
                 print("Something went wrong")
             return -1
 
-    def getItem(self, id: str):
+    async def getItem(self, id: str):
         URL = f"https://edu.pp24.dev/newapi/api/Auction/Get?auctionId={id}"
         while True:
-            if self.__checkNet():
-                resp = self.session.get(URL)
-                break
+            if await self.__checkNet():
+                async with ClientSession(cookie_jar=self.cookies) as session:
+                    async with session.get(URL) as response:
+                        if self.log:
+                            print(await response.text())
+                        resp = await response.json()
+                        self.cookies = session.cookie_jar
+                        return resp
             if self.log:
                 print("Problem with internet")
-        resp = resp.json()  # work with this!
-        return resp
 
-    def getBet(self, id: str, rowversion: str, value: str):
+    async def getBet(self, id: str, rowversion: str, value: str):
         url = "https://edu.pp24.dev/newapi/api/Auction/CreateBet"
         data = {
             'auctionId': id,
@@ -90,7 +105,11 @@ class System:
         }
         while True:
             if self.__checkNet():
-                resp = self.session.post(url, json=data)  # work this!
-                break
+                async with ClientSession(cookie_jar=self.cookies) as session:
+                    async with session.post(url, json=data) as response:
+                        if self.log:
+                            print(await response.text())
+                            self.cookies = session.cookie_jar
+                        break
             if self.log:
                 print("Problem with internet")
