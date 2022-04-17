2c29d5765dcccd7056da1d63ff0af7b2634f60b0
DenchicEz <65415569+denchicez@users.noreply.github.com>
2022-04-17 13:26:29 +0300

    Update Regulator.py

--- Regulator.py
+++ Regulator.py
@@ -1,4 +1,4 @@
-from multiprocessing import Process
+import asyncio
 
 from Player import Player
 
@@ -8,27 +8,27 @@ class Regulator:
         self.system = system
         self.threads = {}
 
-    def play(self, sessionid: str, price: float, timedelta: float, delay: float):  # асинхронная
+    async def play(self, sessionid: str, price: float, timedelta: float, delay: float):  # асинхронная
         result = 0
         player = Player(sessionid, price, timedelta, delay, self.system)
         while (result == 0):
-            result = player.play()
+            result = await player.play()
+        print(f"RESULT IS {result}")
         del self.threads[sessionid]
-        self.system.sender.template(result, sessionid)
+        # self.system.sender.template(result, sessionid)
 
-    def newsession(self, sessionid: str, price: float, sendmessage: float, delay: float):
+    async def newsession(self, sessionid: str, price: float, sendmessage: float, delay: float):
         if (sessionid not in self.threads):
-            self.threads[sessionid] = Process(target=self.play, args=(sessionid, price, sendmessage, delay))
-            self.threads[sessionid].start()
+            self.threads[sessionid] = asyncio.create_task(self.play(sessionid, price, sendmessage, delay))
         else:
             if self.system.log:
                 print("You're now upd this!")
 
-    def getactivities(self):
+    async def getactivities(self):
         return self.threads.keys()
 
-    def kill(self, sessionid: str):
+    async def kill(self, sessionid: str):
         if (sessionid in self.threads):
-            self.threads[sessionid].kill()
+            self.threads[sessionid].cancel()
         else:
             print("Can't find this id to kill someone")
